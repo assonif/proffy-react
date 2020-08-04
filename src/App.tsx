c79c35015687617df7617c5715ab7086ffbccc03
assonif <fabioassoni06@gmail.com>
2020-08-04 16:49:30 -0300

    home page, without web version

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+
 function App() {
-  return <h1>Teste</h1>;
+  return <Landing />;
 }
 
 export default App;
