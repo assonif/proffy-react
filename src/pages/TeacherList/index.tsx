d55337b3ce9400ec3185254dd127e532ed5caee4
assonif <fabioassoni06@gmail.com>
2020-08-07 12:55:50 -0300

    start content 'study'

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 const TeacherList: React.FC = () => {
@@ -23,6 +25,36 @@ const TeacherList: React.FC = () => {
           </div>
         </form>
       </PageHeader>
+      <main>
+        <article>
+          <header>
+            <img src="" alt="" />
+            <div>
+              <strong>Fabio Furtado</strong>
+              <span>Quimica</span>
+            </div>
+          </header>
+
+          <p>
+            shdfsoahdosajdlksajdlakjdslkjdlasjdlkajslkd
+            <br />
+            <br />
+            spajdpojsapods posj dpoajdp ojspojspaojdposa jdpoj psjdpo jpsoaj
+            posjpaojj pojsopj posjp ojspo jspoj pojspoajpoj poja pojp oajp
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsAppIcon} alt="Entrar em contato" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 };
